06c4dac1fd4487ed5f37b282d7020d444098b141
Lee Harding <lee.harding@circuitpeople.com>
2023-04-06 16:14:43 -0700

    Build the lambda, too.

--- scripts/deploy-region.sh
+++ scripts/deploy-region.sh
@@ -2,9 +2,9 @@
 set -e
 
 # regional resources deploy to each supported region...
-aws cloudformation package --template-file templates/regional.template --region us-west-2 --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template-file regional-us-west-2.template.packaged --use-json
-aws cloudformation package --template-file templates/regional.template --region us-east-1 --s3-bucket ${BUCKET_NAME_PREFIX}-us-east-1 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template-file regional-us-east-1.template.packaged --use-json
-aws cloudformation package --template-file templates/regional.template --region eu-west-1 --s3-bucket ${BUCKET_NAME_PREFIX}-eu-west-1 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template-file regional-eu-west-1.template.packaged --use-json
+dotnet lambda package-ci --template templates/regional.template --region us-west-2 --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template regional-us-west-2.template.packaged --use-json
+dotnet lambda package-ci --template templates/regional.template --region us-east-1 --s3-bucket ${BUCKET_NAME_PREFIX}-us-east-1 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template regional-us-east-1.template.packaged --use-json
+dotnet lambda package-ci --template templates/regional.template --region eu-west-1 --s3-bucket ${BUCKET_NAME_PREFIX}-eu-west-1 --s3-prefix ${BUCKET_KEY_PREFIX}/${CODEBUILD_RESOLVED_SOURCE_VERSION} --output-template regional-eu-west-1.template.packaged --use-json
 
 #get global stack output for EB_ROLE_ARN from EventBridgeRoleArn (in us-west-2)
 export EB_ROLE_ARN=$(aws cloudformation describe-stacks --stack-name email-delivery-global --region us-west-2 --query "Stacks[0].Outputs[?OutputKey=='EventBridgeRoleArn'].OutputValue" --output text)
