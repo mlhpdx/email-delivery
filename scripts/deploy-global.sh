83aff5f5515a3975a9cb14939456b8364977e5b0
Lee Harding <lee.harding@circuitpeople.com>
2023-04-05 19:31:19 -0700

    Quote the lists.

--- scripts/deploy-global.sh
+++ scripts/deploy-global.sh
@@ -4,5 +4,5 @@ set -e
 # global resources deploy only in us-west-2!
 
 aws cloudformation package --template-file templates/global.template --s3-bucket ${BUCKET_NAME_PREFIX}-us-west-2 --s3-prefix ${BUCKET_KEY_PREFIX}/email-service/${CODEBUILD_RESOLVED_SOURCE_VERSION} --region us-west-2 > global.template.published
-aws cloudformation deploy --stack-name email-delivery-global --template-file global.template.published --no-fail-on-empty-changeset --parameter-overrides DomainNames=${DOMAIN_NAMES} --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-west-2
+aws cloudformation deploy --stack-name email-delivery-global --template-file global.template.published --no-fail-on-empty-changeset --parameter-overrides DomainNames="${DOMAIN_NAMES}" --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND --region us-west-2
 
